1303fe0b9b38d06e0d16a661195a2a16d5be29fd
chandrakirandandapu18 <chandrakirandandapu18@gmail.com>
2026-07-31 21:55:02 +0530

    STRING_HANDLING.C

--- STRING_HANDLING.C/str_hndlng_concatination.c
+++ STRING_HANDLING.C/str_hndlng_concatination.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 #include<string.h>
-void str_concat()
+void str_concat(char x[20],char y[20])
 {
+    int len,i,j=0;
+    len=strlen(x);
+    for(i=len;y[j]!='\0';i++,j++)
+    {
+        x[i]=y[j];
+    }
+    x[i]='\0';
     
 }
 void main()
 {
-
+    char str1[20]="chandra ";
+    char str2[20]="kiran";
+    printf("Before concating of str1: %s \n",str1);
+    printf("Before concating of str2: %s \n",str2);
+    str_concat(str1,str2);
+    printf("After concating of str1: %s \n",str1);
+    
+    
 }
